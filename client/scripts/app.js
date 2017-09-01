78961d213d7e904d7de0554cc2e17a02fe587436
Victor Wang <victor.wang@me.com>
2017-09-01 14:49:20 -0700

    pass tests up to fetching

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,36 @@
+// $(document).ready(function() {
+//
+//   // API keys: ".. /env/config.js"
+//
+//   $.get(url, function(data) {
+//     console.log()
+//     $('#chats').append(data);
+//   });
+// });
 
-// URL: http://parse.hrsf82.hackreactor.com/chatterbox/classes/messages
+const url = "http://parse.sfm6.hackreactor.com/";
+
+var app = {
+  init() {
+
+  },
+  send(input) {
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: input
+    });
+  },
+  fetch() {
+    $.ajax({
+      type: 'GET'
+    });
+  },
+  clearMessages() {
+    $('#chats').remove();
+  },
+  renderMessage(message) {
+    $('#chats').append(message);
+  }
+
+};
